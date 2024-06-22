8093764fdceb36038e1ae75af0cca39eb4370889
KOTBCTAKAHE <137787433+KOTBCTAKAHE@users.noreply.github.com>
2024-06-23 00:58:52 +0500

    🆕New Daily Combo

--- pages/api/GetCombo.js
+++ pages/api/GetCombo.js
@@ -2,7 +2,7 @@
 
 export default function handler(req, res) {
   res.status(200).json({
-    combo: ["gamefi_tokens", "margin_trading_x30", "trading_bots"],
-    date: "14-06-24"
+    combo: ["anti_money_loundering", "licence_indonesia", "top10_global"],
+    date: "22-06-24"
   });
 }
