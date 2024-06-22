893252e94177fbfaacc3da5da7f4fd3cf7bf68d4
KOTBCTAKAHE <137787433+KOTBCTAKAHE@users.noreply.github.com>
2024-06-23 01:16:47 +0500

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,54 +1,13 @@
-// app 
-import React, { useEffect, useState } from 'react';
-import axios from 'axios';
+// app/page.tsx
+import React from 'react';
+import Combo from '../components/Combo';
 
-interface ComboResponse {
-    combo: string[];
-    date: string;
-}
-
-interface Card {
-    id: string;
-    name: string;
-}
-
-const Combo: React.FC = () => {
-    const [combo, setCombo] = useState<string[]>([]);
-    const [date, setDate] = useState<string>('');
-    const [cardNames, setCardNames] = useState<string[]>([]);
-
-    useEffect(() => {
-        const fetchCombo = async () => {
-            try {
-                const comboResponse = await axios.get<ComboResponse>('https://hamster-kombo-server.vercel.app/api/GetCombo');
-                setCombo(comboResponse.data.combo);
-                setDate(comboResponse.data.date);
-
-                const cardsResponse = await axios.get<Card[]>('https://raw.githubusercontent.com/KOTBCTAKAHE/hamster-kombo-server/dev/allcardids.json');
-                const cardNamesMap: { [key: string]: string } = {};
-                cardsResponse.data.forEach(card => {
-                    cardNamesMap.card.id = card.name;
-                });
-
-                const names = comboResponse.data.combo.map(id => cardNamesMap[id]);
-                setCardNames(names);
-            } catch (error) {
-                console.error('Error fetching combo data:', error);
-            }
-        };
-
-        fetchCombo();
-    }, []);
-
+const HomePage: React.FC = () => {
     return (
         <div>
-            <h1>Вот сегодняшнее комбо!</h1>
-            <div>{cardNames.length > 0 && cardNames.map((name, index) => (
-                <div key={index}>{name}</div>
-            ))}</div>
-            <p>Дата: {date}</p>
+            <Combo />
         </div>
     );
-};
+}
 
-export default Combo;
+export default HomePage;
